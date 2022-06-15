cab6b91c611aeaeaa69004d4b0749e7e95b5d911
Maxim <ranevsky91@mail.ru>
2022-06-15 21:58:07 +0300

    Added solution to codewars 'Greed_is_Good'

---
+++ TasksAndSolutions/Codewars/Greed_is_Good.cs
@@ -0,0 +1,41 @@
+﻿namespace TasksAndSolutions.Codewars;
+
+internal static class Greed_is_Good
+{
+    public static int Score(int[] dice)
+    {
+        byte[] arr = new byte[6];
+        int sum = 0;
+        for (int i = 0; i < dice.Length; i++)
+        {
+            arr[dice[i] - 1]++;
+        }
+
+        if (arr[0] >= 3)
+        {
+            arr[0] -= 3;
+            sum += 1000;
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] >= 3)
+            {
+                arr[i] -= 3;
+                sum += (i + 1) * 100;
+            }
+        }
+
+        if (arr[0] >= 1)
+        {
+            sum += arr[0] * 100;
+        }
+
+        if (arr[4] >= 1)
+        {
+            sum += arr[4] * 50;
+        }
+
+        return sum;
+    }
+}
