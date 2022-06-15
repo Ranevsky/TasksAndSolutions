a53e754c300174d085cc2854f4399ef107ed3c12
Maxim <ranevsky91@mail.ru>
2022-06-15 19:04:45 +0300

    Added solution to codewars 'Sort_the_odd'

---
+++ TasksAndSolutions/Codewars/Sort_the_odd.cs
@@ -0,0 +1,29 @@
+﻿namespace TasksAndSolutions.Codewars;
+internal static class Sort_the_odd
+{
+    public static int[] SortArray(int[] array)
+    {
+        List<int> list = new();
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 1)
+            {
+                list.Add(array[i]);
+            }
+        }
+        
+        list.Sort();
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 1)
+            {
+                array[i] = list[count];
+                count += 1;
+            }
+        }
+        return array;
+    }
+}
