4454caab0012ed887a1d1703de7254b8890816c5
Maxim <ranevsky91@mail.ru>
2022-06-15 21:57:13 +0300

    Added solution to codewars 'Detect_Pangram'

---
+++ TasksAndSolutions/Codewars/Detect_Pangram.cs
@@ -0,0 +1,19 @@
+﻿namespace TasksAndSolutions.Codewars;
+
+internal static class Detect_Pangram
+{
+    const byte countInABC = 26;
+    public static bool IsPangram(string str)
+    {
+        HashSet<char> hash = new(countInABC);
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsLetter(str[i]))
+            {
+                hash.Add(char.ToUpper(str[i]));
+            }
+        }
+
+        return hash.Count == countInABC ? true : false;
+    }
+}
